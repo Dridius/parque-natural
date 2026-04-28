5a9dc6a6e6e78ac007efcd69d2705de085239edb
Adriana Fraga <adrianaffraga@gmail.com>
2026-04-28 14:20:22 +0200

    adicionar musica

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function changeBackgroundToGray() {
   document.body.style.backgroundColor = "gray";
 }
+// Source - https://stackoverflow.com/a/39162154
+// Posted by Surya Teja
+// Retrieved 2026-04-28, License - CC BY-SA 3.0
+
+function playMusic() {
+  var music = new Audio("mixkit-morning-birds-2472.wav");
+  music.play();
+}
+//https://mixkit.co/free-sound-effects/discover/mp3/
